d982e2132caa3cca4bb6b06389031f961549202e
Ignacio Cuñado Barral <116840139+ignaciocunado@users.noreply.github.com>
2023-01-17 19:27:32 +0100

    Updated sizes

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-window.onload = start;
+// Test 1:
 
-function start() {
-    
-}
+// window.onload = change()
+
+// function changeadjectives() {
+//     while(true) {
+//         setTimeout(change, 5000);
+//         setTimeout(change2, 5000);
+//     }
+//     function change() {
+//         document.getElementById("adjectives").innerHTML = "Curious, Resilient and Enthusiastic";
+//     }
+//     function change2() {
+//         document.getElementById("adjectives").innerHTML = "Leader, Team-Worker and Methodical";
+//     }
+// }
+
+// Test 2:
+
+// var text = ["Curious, Resilient and Enthusiastic", "Leader, Team-Worker and Methodical"];
+// var counter = 0;
+// var elem = document.getElementById("adjectives");
+// var inst = setInterval(change, 5000);
+
+// function change() {
+//   elem.innerHTML = text[counter];
+//   counter++;
+//   if (counter >= text.length) {
+//     counter = 0;
+//     // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+//   }
+// }
+
