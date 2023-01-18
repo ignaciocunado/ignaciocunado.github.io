2c358fbd5523c720deee22f3746d5819a0c60403
Ignacio Cuñado Barral <116840139+ignaciocunado@users.noreply.github.com>
2023-01-18 01:19:23 +0100

    Added info
    Added info about the web being hosted in web3 and set a background color for the footer.

--- script.js
+++ script.js
@@ -17,17 +17,17 @@
 
 // Test 2:
 
-// var text = ["Curious, Resilient and Enthusiastic", "Leader, Team-Worker and Methodical"];
-// var counter = 0;
-// var elem = document.getElementById("adjectives");
-// var inst = setInterval(change, 5000);
+var text = ["Curious, Resilient and Enthusiastic", "Leader, Team-Worker and Methodical"];
+var counter = 0;
+var elem = document.getElementById("adjectives");
+var inst = setInterval(change, 5000);
 
-// function change() {
-//   elem.innerHTML = text[counter];
-//   counter++;
-//   if (counter >= text.length) {
-//     counter = 0;
-//     // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
-//   }
-// }
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
 
