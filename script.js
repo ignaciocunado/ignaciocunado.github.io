bc22aaa352abd72dded595476ff4dec06962a9cf
Ignacio Cuñado Barral <116840139+ignaciocunado@users.noreply.github.com>
2023-01-19 13:13:04 +0100

    Added the correct JS to make the text under name change

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
 // Test 1:
 
-// window.onload = change()
+window.onload = changeadjectives();
 
-// function changeadjectives() {
-//     while(true) {
-//         setTimeout(change, 5000);
-//         setTimeout(change2, 5000);
-//     }
-//     function change() {
-//         document.getElementById("adjectives").innerHTML = "Curious, Resilient and Enthusiastic";
-//     }
-//     function change2() {
-//         document.getElementById("adjectives").innerHTML = "Leader, Team-Worker and Methodical";
-//     }
-// }
-
-// Test 2:
-
-var text = ["Curious, Resilient and Enthusiastic", "Leader, Team-Worker and Methodical"];
-var counter = 0;
-var elem = document.getElementById("adjectives");
-var inst = setInterval(change, 5000);
-
-function change() {
-  elem.innerHTML = text[counter];
-  counter++;
-  if (counter >= text.length) {
-    counter = 0;
-    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
-  }
+function changeadjectives() {
+    const arr = ["Leader, Team-Worker and Methodical","Curious, Resilient and Enthusiastic"];
+    var counter = 0;
+    setInterval(change, 3000);
+    function change() {
+      document.getElementById("adjectives").innerHTML = arr[counter];
+      counter++;
+      if(counter >= arr.length) {
+        counter = 0;
+      }
+    }
 }
-
